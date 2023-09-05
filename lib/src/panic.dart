3cabfaea51a8963cc28f7f41c3adfe407b454d66
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-09-05 10:14:30 +0330

    [Minor+Docs]<Panic> Error is now non-nullable

--- lib/src/panic.dart
+++ lib/src/panic.dart
@@ -1,11 +1,19 @@
+/// {@template panic}
 /// An Unhandled exception that did not get caught inside a deffer statement
+/// {@endtemplate}
 class Panic implements Exception {
+  /// {@macro panic}
   Panic({
     required this.message,
     required this.error,
   });
 
-  final Object? error;
+  /// Object that forced this panic to happen
+  ///
+  /// normally item
+  final Object error;
+
+  /// Panic message
   final String message;
   @override
   String toString() {
