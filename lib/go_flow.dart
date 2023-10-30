c60033e3997050a559ca061e93cd412cc2af2be4
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-10-30 11:32:37 +0330

    [Minor]

--- lib/go_flow.dart
+++ lib/go_flow.dart
@@ -1,4 +1,4 @@
-/// This package aims to bring a deferred execution mechanism similar to 
+/// This package aims to bring a deferred execution mechanism similar to
 /// Golang's defer statement into the Dart programming language.
 /// This mechanism allows you to schedule code to run when a certain scope or
 /// function exits, ensuring that necessary cleanup or finalization tasks are
